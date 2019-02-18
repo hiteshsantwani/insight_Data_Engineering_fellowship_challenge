f6a7f14af35077ecd51eb7410c0eb87cdedab4cb
hiteshsantwani <hiteshsantwani35@gmail.com>
2019-02-17 21:37:33 -0500

    Refactoring and Validation using pandas

--- src/pharmacy_counting_using_pandas.py
+++ src/pharmacy_counting_using_pandas.py
@@ -3,6 +3,7 @@
 # Load the Pandas libraries with alias 'pd'
 
 import pandas as pd
+import numpy as np
 
 input_file_df = pd.read_csv("/Users/hiteshsantwani/Desktop/"
                          "Insight Fellowship Coding challenge/MySolution2/insight_Data_Engineering_fellowship_challenge/"
@@ -18,5 +19,9 @@ mapping = {
 print(input_file_df.head())
 
 input_file_df = input_file_df.set_index('id').groupby(mapping, axis=1).sum()
-input_file_df.name = input_file_df.groupby('drug_name').name.count()
-input_file_df.drug_cost = input_file_df.groupby('drug_name').drug_cost.sum()
+input_file_df_drug_cost = input_file_df.groupby('drug_name')['drug_cost'].agg([np.sum])
+input_file_df_drug_cost['name'] = input_file_df.groupby('drug_name').name.unique()
+input_file_df_drug_cost['num_prescriber'] = input_file_df_drug_cost["name"].apply(lambda x: len(x))
+input_file_df = input_file_df_drug_cost.drop(columns = ['name']).reset_index().rename(columns = {'sum':'total_cost'})
+input_file_df = input_file_df[['drug_name', 'num_prescriber', 'total_cost']]
+pass
